0964a589c06198c38e756d3e5f7091bef8601847
Laura Camargo <camargolauraa@gmail.com>
2025-10-20 22:37:37 -0300

    feat: págima home signedin

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,22 +1,112 @@
-import React from "react";
-
-import { StyleSheet, View } from "react-native";
-
 import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 import Colors from "@/app/constants/Colors";
 import FooterSignedUp from "@/components/FooterSignedUp";
 import HeaderPrincipal from "@/components/HeaderPrincipal";
 
-export default function SignIn() {
+// Constante com dados de exemplo para a lista de produtos
+const DUMMY_PRODUCTS = [
+  {
+    id: "1",
+    name: "NOME DO PRODUTO",
+    category: "Categoria",
+    brand: "MARCA X LTDA",
+    lote: "00000",
+    validade: "dd/mm/aaaa",
+    hasWarning: true,
+    isChecked: true,
+  },
+  {
+    id: "2",
+    name: "NOME DO PRODUTO",
+    category: "Categoria",
+    brand: "MARCA X LTDA",
+    lote: "00000",
+    validade: "dd/mm/aaaa",
+    hasWarning: false,
+    isChecked: true,
+  },
+  {
+    id: "3",
+    name: "NOME DO PRODUTO",
+    category: "Categoria",
+    brand: "MARCA X LTDA",
+    lote: "00000",
+    validade: "dd/mm/aaaa",
+    hasWarning: true,
+    isChecked: false,
+  },
+];
+
+export default function Home() {
   const navigation = useNavigation<any>();
 
   return (
     <View style={styles.background}>
       <HeaderPrincipal />
+      {/* Bloco de Informações do Administrador */}
+      <View style={styles.adminInfoContainer}>
+        <Text style={styles.adminName}>ADMINISTRADOR - Nome</Text>
+        <Text style={styles.companyDetails}>Empresa XXXXXXX XXXXXX</Text>
+        <Text style={styles.companyDetails}>CNPJ: XX.XXX.XXX/XXXX-XX</Text>
+      </View>
+      {/* Bloco de Navegação */}
+      <View style={styles.listHeader}>
+        <Text style={styles.listTitle}>Meus produtos:</Text>
+        <View style={styles.actionIcons}>
+          <Pressable>
+            <Image source={require("../assets/images/plusIcon.png")} />
+          </Pressable>
+          <Pressable>
+            <Image source={require("../assets/images/searchIcon.png")} />
+          </Pressable>
+          <Pressable>
+            <Image source={require("../assets/images/filterIcon.png")} />
+          </Pressable>
+        </View>
+      </View>
+
+      {/* ScrollView */}
+
+      <ScrollView contentContainerStyle={styles.container}>
+        {/* Bloco da Lista de Produtos */}
+
+        {DUMMY_PRODUCTS.map((product) => (
+          <View key={product.id} style={styles.productCard}>
+            <View style={styles.productInfo}>
+              <Text style={styles.productName}>{product.name}</Text>
+              <Text style={styles.productCategory}>{product.category}</Text>
+              <Text style={styles.productDetails}>Marca: {product.brand}</Text>
+              <Text style={styles.productDetails}>Lote: {product.lote}</Text>
+              <Text style={styles.productDetails}>
+                Validade: {product.validade}
+              </Text>
+            </View>
+
+            <View style={styles.productStatusIcons}>
+              {product.hasWarning && (
+                <Image source={require("../assets/images/alertIcon.png")} />
+              )}
+
+              {product.isChecked && (
+                <Image source={require("../assets/images/checkIcon.png")} />
+              )}
+            </View>
+          </View>
+        ))}
+      </ScrollView>
 
       {/* Rodapé */}
-      <View style={{ position: "absolute", left: 0, right: 0, bottom: 0 }}>
+      <View style={styles.footerContainer}>
         <FooterSignedUp />
       </View>
     </View>
@@ -27,73 +117,94 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: Colors.background,
-    justifyContent: "center",
+    paddingTop: 120,
+  },
+  container: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 10,
+    paddingTop: 15,
+    paddingBottom: "120%",
+    borderWidth: 1,
+    borderTopColor: Colors.primary,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  adminInfoContainer: {
+    backgroundColor: "#fff",
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.primary,
+  },
+  adminName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: Colors.greyText,
+    marginBottom: 15,
+  },
+  companyDetails: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: Colors.greyText,
+  },
+  productsSection: {
+    marginTop: 20,
+  },
+  listHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    marginVertical: 10,
+    paddingHorizontal: 20,
   },
-  headerContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 77,
+  listTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: Colors.primary,
   },
-  box: {
-    width: "85%",
+  actionIcons: {
+    flexDirection: "row",
+    gap: 35,
+    backgroundColor: "#fff",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 30,
+  },
+  productCard: {
     backgroundColor: "#fff",
     borderRadius: 15,
-    borderColor: Colors.primary,
+    padding: 15,
+    marginBottom: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderWidth: 1,
-    padding: 25,
-    alignItems: "center",
-    gap: 20,
+    borderColor: Colors.primary,
   },
-  title: {
-    fontSize: 28,
+  productInfo: {
+    flex: 1,
+  },
+  productName: {
+    fontSize: 16,
     fontWeight: "bold",
-    color: Colors.greyText,
+  },
+  productCategory: {
+    fontSize: 14,
+    color: "grey",
     marginBottom: 10,
   },
-  input: {
-    width: "100%",
-    backgroundColor: Colors.secondary,
-    color: "#fff",
-    paddingHorizontal: 15,
-    paddingVertical: 12,
-    borderRadius: 8,
-    fontSize: 16,
-  },
-  errorText: {
-    alignSelf: "flex-start",
-    color: "#ff3333",
+  productDetails: {
     fontSize: 12,
-    marginTop: 6,
-    marginBottom: -4,
+    color: "grey",
   },
-  signinContainer: {
+  productStatusIcons: {
     flexDirection: "row",
-    marginTop: 5,
+    alignItems: "flex-start",
+    gap: 8,
   },
-  signinText: {
-    fontSize: 12,
-    color: Colors.greyText,
-    fontWeight: "bold",
-  },
-  signinLink: {
-    fontSize: 14,
-    color: Colors.primary,
-    fontWeight: "bold",
-  },
-  button: {
-    backgroundColor: Colors.primary,
-    paddingHorizontal: 39,
-    paddingVertical: 10,
-    borderRadius: 8,
-    alignItems: "center",
-    marginTop: 15,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
+  footerContainer: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
